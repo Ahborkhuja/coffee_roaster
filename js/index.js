8dd57d90206afeb29bc7a924493ff464ad62405c
ahborkhuja <93854287+Ahborkhuja@users.noreply.github.com>
2022-09-22 15:09:58 +0500

    upd

--- js/index.js
+++ js/index.js
@@ -1,12 +1,29 @@
-var making_steps = document.querySelectorAll('.making_steps li');
-var rotating = document.querySelectorAll('.img_rotating'); 
-
-making_steps.forEach(li=> {
-  li.addEventListener('click',()=> {
-    li.classList.toggle('actstep');
-    rotating.classList.toggle('rotating_img');
+var making_steps = document.querySelectorAll('.coffee_making__menu_list li');
+var rotating = document.querySelectorAll('.content__row img');
+const mobile_header = document.querySelector('.site_header__burger');
+const body = document.querySelector('body');
+making_steps.forEach(li => {
+  li.addEventListener('click', () => {
+    li.classList.add('actstep');
+    resetSteps();
+    li.classList.add('actstep');
   });
 });
-function resetSteps(){
-  making_steps.forEach(li=> li.classList.remove('actstep'));
+function resetSteps() {
+  making_steps.forEach(li => li.classList.remove('actstep'));
 }
+rotating.forEach(img =>{
+  img.addEventListener('click', () => {
+    img.classList.add('rotor');
+    rotar();
+    img.classList.add('rotor');
+  });
+});
+function rotar() {
+  rotating.forEach(img => img.classList.remove('rotor'));
+};
+mobile_header.addEventListener('click', ()=>{
+  mobile_header.classList.toggle('open'
+  );
+  body.classList.toggle('overflow');
+});
